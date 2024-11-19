b901cf9f38c523449e673534038315b3950eccba
younesscoder01 <younesscoder@gmail.com>
2024-11-19 09:55:45 +0100

    add the name of the window

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
         printf("%s\n", map[i]);
     }
     mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, get_map_weight(map) * 30, get_map_height(map) * 30, "Hello world!");
+    mlx_win = mlx_new_window(mlx, get_map_weight(map) * 30, get_map_height(map) * 30, "PixelFlood");
     render_wall(map, mlx_win, mlx);
     render_floor(map, mlx_win, mlx);
     flood_fill(map, get_player_x(map), get_player_y(map), get_map_height(map), get_map_weight(map), (param){mlx, mlx_win});
